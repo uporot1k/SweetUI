2862e40292ea8e2a51fe7e90f7b0eda616a8348f
Egor <uporot1k@yandex.ru>
2019-09-02 17:54:15 +0300

    refactor && add component props

--- src/components/SBtn/SBtn.vue
+++ src/components/SBtn/SBtn.vue
@@ -1,28 +1,34 @@
-<template>
-  <component :is="tag? tag : 'button'" :class='classes' :style="styleObj">
-    <div class="s-btn__text">
-      <slot/>
-    </div>
-    <div class="s-btn__icon" v-if="icon != ''">
-      <s-icon :iconFill="fill" icon="search"></s-icon>
-    </div>
-    
-  </component>
+<template lang="pug">
+  component(
+    :is="tag"
+    :class="classes"
+  )
+    .s-btn__text
+      slot
+    .s-btn__icon.icon(
+      :class="[iconPosition === 'left' ? 'icon--left' : 'icon--right']"
+    )  
+      s-icon(
+        icon="search"
+      )  
 </template>
 <script>
-  import mixins from '../../util/mixins';
-  import colorable from '../../mixins/colorable/index'
-  import Vue from 'vue'
-
+  import './SBtn.styl'
 
   export default {
-    mixins: [colorable],
     name: 'SBtn',
     props: {
-      size: String || Object,
+      tag: {
+        type: String,
+        default: 'button'
+      },
+      size: {
+        type: [String, Object],
+        default: 'big'
+      },
+      type: String,
       icon: String,
       iconFill: String, 
-      tag: String,
       iconPosition: String
     },
     data() {
@@ -36,17 +42,18 @@
         return {
           's-btn': true,
           's-control': true,
-          's-control--small': this.size == 'small' ? true : false,
-          's-control--big': !this.size ? true : false,
-          's-control--disabled': this.$attrs.disabled == ''? true : false,
+          's-btn--small': this.size === 'small',
+          's-btn--big': this.size === 'big',
+          's-btn--disabled': this.$attrs.disabled ? true : false,
+          's-btn--primary': !this.type !== '' ? true : false,
+          's-btn--outlined': this.$attrs.outlined ===true,
+          ...this.type
         }
       },
-      styleObj() {
-        return {}
-      }
     },
 
     created() {
+      console.log(this.$attrs)
       if(this.iconFill === undefined) {
         console.log(1)
         this.fill = this.color;
@@ -57,20 +64,3 @@
     }
   }
 </script>
-<style lang="stylus">
-@import '../../assets/stylus/main.styl'
-.s-btn
-  display flex
-  align-items center
-  justify-content center
-  min-width 144px
-  background-color $color-cornflower
-  border-radius 6px
-  padding 0 8px
-  user-select none
-  font-size 16px
-  &__text
-    padding: 0 8px
-  &:focus
-    outline none
-</style>
