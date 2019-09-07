1fedbd8337d722ee099b722f129c0f0bd87c1f43
Egor <uporot1k@yandex.ru>
2019-09-07 21:00:27 +0300

    master

--- .storybook/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-// Export a function. Accept the base config as the only param.
-const custom = require('../build/webpack.base.conf');
-
-module.exports = async ({ config, mode }) => {
-  return { ...config, module: { ...config.module, rules: custom.module.rules } };
-};
