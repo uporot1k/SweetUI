60d176150b737191faab4bb83857eb7694942d30
Egor <uporot1k@yandex.ru>
2019-09-20 23:33:50 +0300

    0.0.0-rc.11

--- rollup.config.js
+++ rollup.config.js
@@ -1,181 +1,69 @@
-import vue from 'rollup-plugin-vue'
-import node from 'rollup-plugin-node-resolve'
-import cjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
-import { terser } from 'rollup-plugin-terser'
-
-import fs from 'fs'
-import path from 'path'
-
-import pack from './package.json'
-
-const babelConfig = {
-    exclude: 'node_modules/**',
-    runtimeHelpers: true,
-    babelrc: false,
-    presets: [['@babel/preset-env', { modules: false }]]
-}
-
-const bannerTxt = `/*! SweetUI v${pack.version} | MIT License | github.com/uporot1k/SweetUI */`
-
-const baseFolder = './src/'
-const componentsFolder = 'components/'
-
-const components = fs
-    .readdirSync(baseFolder + componentsFolder)
-    .filter((f) => {
-    console.log(baseFolder, componentsFolder, f);
-        return fs.statSync(path.join(baseFolder + componentsFolder, f)).isDirectory()
-    })
-
-const entries = {
-    'index': './src/index.js',
-    ...components.reduce((obj, name) => {
-        obj[name] = (baseFolder + componentsFolder + name)
-        return obj
-    }, {})
-}
+import commonjs from 'rollup-plugin-commonjs'; // Конвертирование CommonJS модулей в ES6
+import vue from 'rollup-plugin-vue'; // Обработка однофайловых компонентов .vue
+import buble from 'rollup-plugin-buble'; // Транспиляция/добавление полифилов для умеренной поддержки браузеров
 
 const capitalize = (s) => {
-    if (typeof s !== 'string') return ''
-    return s.charAt(0).toUpperCase() + s.slice(1)
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
-export default () => {
-    const mapComponent = (name) => {
-        return [
-            {
-                input: baseFolder + componentsFolder + `${name}/index.js`,
-                external: ['vue'],
-                output: {
-                    format: 'umd',
-                    name: capitalize(name),
-                    file: `dist/components/${name}/index.js`,
-                    banner: bannerTxt,
-                    exports: 'named',
-                    globals: {
-                        vue: 'Vue'
-                    }
-                },
-                plugins: [
-                    node({
-                        extensions: ['.vue', '.js']
-                    }),
-                    cjs(),
-                    vue({
-                        template: {
-                            isProduction: true
-                        }
-                    }),
-                    babel(babelConfig)
-                ]
-            }
-        ]
-    }
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      format: 'esm',
+      file: 'dist/library.esm.js',
+      name: capitalize('test'),
+    },
+    plugins: [
+      commonjs(),
+      vue({
+          css: true, // Динамически внедряем CSS в тег <style>
+          compileTemplate: true, // Явное преобразование шаблона в рендер-функцию
+      }),
+      buble() // Транспиляция в ES5
+    ],
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      format: 'iife',
+      file: 'dist/library.js',
+      name: capitalize('test'),
+    },
+    plugins: [
+      commonjs(),
+      vue({
+          css: true, // Динамически внедряем CSS в тег <style>
+          compileTemplate: true, // Явное преобразование шаблона в рендер-функцию
+      }),
+      buble() // Транспиляция в ES5
+    ],
+  },
+];
 
-    let config = [
-        {
-            input: entries,
-            external: ['vue'],
-            output: {
-                format: 'esm',
-                dir: `dist/esm`
-            },
-            plugins: [
-                node({
-                    extensions: ['.vue', '.js']
-                }),
-                vue({
-                    template: {
-                        isProduction: true
-                    }
-                }),
-                babel(babelConfig),
-                cjs()
-            ]
-        },
-        {
-            input: entries,
-            external: ['vue'],
-            output: {
-                format: 'cjs',
-                dir: 'dist/cjs',
-                exports: 'named'
-            },
-            plugins: [
-                node({
-                    extensions: ['.vue', '.js']
-                }),
-                vue({
-                    template: {
-                        isProduction: true
-                    }
-                }),
-                babel(babelConfig),
-                cjs()
-            ]
-        },
-        {
-            input: 'src/index.js',
-            external: ['vue'],
-            output: {
-                format: 'umd',
-                name: capitalize('sweet-ui'),
-                file: 'dist/sweet-ui.js',
-                exports: 'named',
-                banner: bannerTxt,
-                globals: {
-                    vue: 'Vue'
-                }
-            },
-            plugins: [
-                node({
-                    extensions: ['.vue', '.js']
-                }),
-                vue({
-                    template: {
-                        isProduction: true
-                    }
-                }),
-                babel(babelConfig),
-                cjs()
-            ]
-        },
-        {
-            input: 'src/index.js',
-            external: ['vue'],
-            output: {
-                format: 'esm',
-                file: 'dist/sweet-ui.esm.js',
-                banner: bannerTxt
-            },
-            plugins: [
-                node({
-                    extensions: ['.vue', '.js']
-                }),
-                vue({
-                    template: {
-                        isProduction: true
-                    }
-                }),
-                babel(babelConfig),
-                cjs()
-            ]
-        },
-        // individual components
-        ...components.map((f) => mapComponent(f)).reduce((r, a) => r.concat(a), [])
-    ]
 
-    if (process.env.MINIFY === 'true') {
-        config = config.filter((c) => !!c.output.file)
-        config.forEach((c) => {
-            c.output.file = c.output.file.replace(/\.js/g, '.min.js')
-            c.plugins.push(terser({
-                output: {
-                    comments: '/^!/'
-                }
-            }))
-        })
-    }
-    return config
-}
+// import vue from 'rollup-plugin-vue'
+
+// export default  [
+//   {
+//     input: 'src/index.js',
+//     output: {
+//       format: 'esm',
+//       file: 'dist/library.esm.js'
+//     },
+//     plugins: [
+//       vue()
+//     ]
+//   },
+//   {
+//     input: 'src/index.js',
+//     output: {
+//       format: 'iife',
+//       file: 'dist/library.js'
+//     },
+//     plugins: [
+//       vue()
+//     ]
+//   },
+// ]
--- src/index.js
+++ src/index.js
@@ -1,29 +1,63 @@
 import * as components from './components'
 
-import config, { setOptions } from './util/config'
-import { use, registerComponentProgrammatic } from './util/plugins'
+// import config, { setOptions } from './util/config'
+// import { use, registerComponentProgrammatic } from './util/plugins'
 
-const SweetUI = {
-    install(Vue, options = {}) {
-        // Options
-        setOptions(Object.assign(config, options))
-        // Components
-        for (let componentKey in components) {
-            Vue.use(components[componentKey])
-        }
-        // Config component
-        const SweetUIProgrammatic = {
-            setOptions(options) {
-                setOptions(Object.assign(config, options))
-            }
-        }
-        registerComponentProgrammatic(Vue, 'config', SweetUIProgrammatic)
-    }
+// // Импорт vue компонента
+// import component from './my-component.vue';
+
+// Объявление функции установки, выполняемой Vue.use()
+export function install(Vue) {
+  for (let componentKey in components) {
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component(componentKey, components[componentKey]);
+  }
 }
 
-use(SweetUI)
+// Создание значения модуля для Vue.use()
+const plugin = {
+  install
+};
 
-export default SweetUI
+// Автоматическая установка, когда vue найден (например в браузере с помощью тега <script>)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+// Экспорт компонента, для использования в качестве модуля (npm/webpack/etc.)
+// export default component;
+
+export * from './components'
+
+
+// const SweetUI = {
+//     install(Vue, options = {}) {
+//         // Options
+//         setOptions(Object.assign(config, options))
+//         // Components
+//         for (let componentKey in components) {
+//             Vue.use(components[componentKey])
+//         }
+//         // Config component
+//         const SweetUIProgrammatic = {
+//             setOptions(options) {
+//                 setOptions(Object.assign(config, options))
+//             }
+//         }
+//         registerComponentProgrammatic(Vue, 'config', SweetUIProgrammatic)
+//     }
+// }
+
+// use(SweetUI)
+
+// export default SweetUI
 
 // export all components as vue plugin
-export * from './components'
+
