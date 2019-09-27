a1e1948c2631e7984f019014a555eed7d344ef15
Egor <uporot1k@yandex.ru>
2019-09-27 17:17:40 +0300

    test

--- src/components/SSwitcher/index.js
+++ src/components/SSwitcher/index.js
@@ -1,1 +1,2 @@
 export { default as SSwitcher } from './SSwitcher.vue'
+export { default as SSwitcherControl } from './SSwitcherControl.vue'
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as SSwitcher } from './SSwitcher/SSwitcher.vue'
+export { default as SSwitcherControl } from './SSwitcher/SSwitcherControl.vue'
 export { default as SIcon } from './SIcon/SIcon.vue'
 export { default as SBtn } from './SBtn/SBtn.vue'
